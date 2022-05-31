f93031675073f5991a8f41d993b014714d446e49
Ba Thi Truong <ba.truongthi@asnet.com.vn>
2022-05-31 15:36:26 +0700

    Add amplify auth module for app-1, app-2

--- app-1/src/main.ts
+++ app-1/src/main.ts
@@ -4,9 +4,18 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
 
+import Auth from '@aws-amplify/auth';
+import AWSConfig from './aws-exports';
+
+Auth.configure({
+  ...AWSConfig,
+  aws_user_pools_web_client_id: '3kd5ijj0a7e8kc2unsm3unjr5j',
+});
+
 if (environment.production) {
   enableProdMode();
 }
 
-platformBrowserDynamic().bootstrapModule(AppModule)
-  .catch(err => console.error(err));
+platformBrowserDynamic()
+  .bootstrapModule(AppModule)
+  .catch((err) => console.error(err));
